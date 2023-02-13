a7102777a7d979bf69762307a00b8980510062cf
Akhilesh Tyagi <akhilesh@hungerbox.com>
2023-02-13 12:35:03 +0530

    Use for loop in template

--- website/views.py
+++ website/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting
 
 # Create your views here.
 
 def welcome(request):
     return render(request, "mywebsite/welcome.html",
-                  {"aKey": "Value"})
+                  {"meetings": Meeting.objects.all()})
 
 def welcome_date(request):
     return HttpResponse("Hey there. You are really welcome here!!... " + str(datetime.now()))
