6091eb634f2a153b2bf4a00e7ac20bfa313ee4d8
Akhilesh Tyagi <akhilesh@hungerbox.com>
2023-02-13 17:44:11 +0530

    Add form post request handling

--- meetings/views.py
+++ meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
 from django.forms import modelform_factory
 
@@ -17,5 +17,11 @@ MeetingForm = modelform_factory(Meeting, exclude=[])
 
 
 def new(request):
-    form = MeetingForm()
-    return render(request, "meetings/new.html", {"form": form})
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = MeetingForm()
+        return render(request, "meetings/new.html", {"form": form})
