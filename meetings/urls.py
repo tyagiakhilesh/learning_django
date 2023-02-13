ff542a9526bec096c964c81c52ea83c82cfe8d05
Akhilesh Tyagi <akhilesh@hungerbox.com>
2023-02-13 17:35:13 +0530

    Add django form with csrf token; POST API not implemented yet

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import details, all_rooms
+from .views import details, all_rooms, new
 
 urlpatterns = [
     path('<int:id>', details, name='meeting_detail'),
-    path('rooms', all_rooms, name='all_rooms')
+    path('rooms', all_rooms, name='all_rooms'),
+    path('new', new, name='new_meeting')
 ]
